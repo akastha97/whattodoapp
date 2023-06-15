62137ca089d2cbbe31e12333867222a38583043d
Aishwarya Kasthala <akasthala97@gmail.com>
2023-06-15 18:35:07 -0500

    initial commit

---
+++ whattodo_final/README.md
@@ -0,0 +1,3 @@
+# whattodo_final
+
+A new Flutter project.
---
+++ whattodo_final/android/app/src/main/kotlin/com/example/whattodo_final/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.whattodo_final
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
